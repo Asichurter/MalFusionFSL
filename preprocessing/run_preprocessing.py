91a2f4a852f20de50a781055ae0dd762940a5b96
Asichurter <1090489718@qq.com>
2021-01-02 00:02:52 +0800

    rename

--- preprocessing/run_preprocessing.py
+++ preprocessing/run_preprocessing.py
@@ -1,6 +1,8 @@
+from preprocessing.dataset.collect import collectPEwithAPI
+from preprocessing.dataset.sample import sampleClassWiseData
 from preprocessing.normalization import removeAPIRedundancy
 from preprocessing.ngram import statNGramFrequency, mapAndExtractTopKNgram
-from preprocessing.wdembedding import trainGloVe
+from preprocessing.word_embedding import trainGloVe
 from preprocessing.dataset.structure import makeDatasetDirStruct
 from preprocessing.dataset.rename import renamePEbyMD5fromApi
 from preprocessing.image import convertDir2Image, convert
@@ -25,12 +27,16 @@ from utils.manager import PathManager
 #                        class_dir=True,
 #                        map_dump_path='/home/omnisky/NewAsichurter/FusionData/reports/Per40_3gram_map.json')
 
-# makeDatasetDirStruct(base_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per40/')
-
-# trainGloVe(base_path='/home/omnisky/NewAsichurter/FusionData/datasets/',
-#            dataset='LargePE-Per40',
-#            size=300,
-#            type='all')
+# makeDatasetDirStruct(base_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per35/')
+#
+# sampleClassWiseData(dst_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per35/all/api/',
+#                     log_file_path="/home/omnisky/NewAsichurter/FusionData/reports/class_stat_after_processing_log.json",
+#                     num_per_class=35)
+#
+trainGloVe(base_path='/home/omnisky/NewAsichurter/FusionData/datasets/',
+           dataset='LargePE-Per35',
+           size=300,
+           type='all')
 
 # renamePEbyMD5fromApi(api_dir_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per40/all/api/',
 #                      pe_dir_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per40/all/pe/')
@@ -38,11 +44,11 @@ from utils.manager import PathManager
 # convertDir2Image(dir_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per40/PEs/',
 #                  dst_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per40/all/img/')
 
-# splitDataset(dataset_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per40/',
-#              validate_ratio=30,
-#              test_ratio=30)
+splitDataset(dataset_path='/home/omnisky/NewAsichurter/FusionData/datasets/LargePE-Per35/',
+             validate_ratio=30,
+             test_ratio=30)
 
-packAllSubsets("LargePE-Per40", num_per_class=40, max_seq_len=300)
+packAllSubsets("LargePE-Per35", num_per_class=35, max_seq_len=300)
 
 
 
