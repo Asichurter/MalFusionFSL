c0e32a33cfab8f84b071c6954ae4ec127bee25e5
Asichurter <Asichurter@users.noreply.github.com>
2021-01-11 00:19:56 +0800

    remove some useless code

--- comp/nn/embedding/sequential.py
+++ comp/nn/embedding/sequential.py
@@ -40,6 +40,7 @@ class BiLstmEncoder(nn.Module):
 
     def forward(self, x, lens):
         if not isinstance(x, t.nn.utils.rnn.PackedSequence) and lens is not None:
+            # 由于collect的时候并没有按照长度顺序进行排序，因此此处需要设置enfore_sorted为False
             x = pack_padded_sequence(x, lens, batch_first=True, enforce_sorted=False)
 
         # 进入lstm前先pack
@@ -68,65 +69,4 @@ class BiLstmEncoder(nn.Module):
         if self.Sequential:
             return out, lens
         else:
-            return out
-
-    @staticmethod
-    def permute_hidden(hx, permutation):
-        # type: (Tuple[Tensor, Tensor], Optional[Tensor]) -> Tuple[Tensor, Tensor]
-        if permutation is None:
-            return hx
-        return BiLstmEncoder.apply_permutation(hx[0], permutation), \
-               BiLstmEncoder.apply_permutation(hx[1], permutation)
-
-    @staticmethod
-    def apply_permutation(tensor, permutation, dim=1):
-        # type: (Tensor, Tensor, int) -> Tensor
-        return tensor.index_select(dim, permutation)
-
-    #################################################
-    # 使用给定的参数进行forward
-    #################################################
-    def static_forward(self, x, lens, params):           # PyTorch1.4目前不支持在rnn上多次backward
-        packed = isinstance(x, t.nn.utils.rnn.PackedSequence)
-        if not packed and lens is not None:
-            x = pack_padded_sequence(x, lens, batch_first=True, enforce_sorted=False)
-
-        x, batch_sizes, sorted_indices, unsorted_indices = x
-        max_batch_size = int(batch_sizes[0])
-
-        num_directions = 2
-        zeros = t.zeros(self.Encoder.num_layers * num_directions,
-                            max_batch_size, self.Encoder.hidden_size,
-                            dtype=x.dtype, device=x.device)
-        hx = (zeros, zeros)
-
-        weights = [params['Encoder.Encoder.weight_ih_l0'],
-                   params['Encoder.Encoder.weight_hh_l0'],
-                   params['Encoder.Encoder.bias_ih_l0'],
-                   params['Encoder.Encoder.bias_hh_l0'],
-                   params['Encoder.Encoder.weight_ih_l0_reverse'],
-                   params['Encoder.Encoder.weight_hh_l0_reverse'],
-                   params['Encoder.Encoder.bias_ih_l0_reverse'],
-                   params['Encoder.Encoder.bias_hh_l0_reverse']
-                   ]
-
-        result = _VF.lstm(x, batch_sizes, hx,
-                          weights,
-                          True,     # 是否bias
-                          self.Encoder.num_layers,
-                          self.Encoder.dropout,
-                          self.Encoder.training,
-                          self.Encoder.bidirectional)
-
-        out, h = result[0], result[1:]
-
-        out = PackedSequence(out, batch_sizes, sorted_indices, unsorted_indices)
-        h = BiLstmEncoder.permute_hidden(h, unsorted_indices)
-
-        if self.Attention is not None:
-            out = BiliAttnReduction.static_forward(out, params)
-            return out
-        else:
-            # TODO: 由于使用了CNN进行解码，因此还是可以返回整个序列
-            out, lens = pad_packed_sequence(out, batch_first=True)
             return out
