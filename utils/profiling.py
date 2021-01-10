be246b80a69363c93fb3efcda98bb4e1e24a61b5
Asichurter <Asichurter@users.noreply.github.com>
2021-01-11 00:27:49 +0800

    add function profiling decorator class

--- utils/profiling.py
+++ utils/profiling.py
@@ -45,11 +45,51 @@ class ClassProfiler:
 
             if iters % report_iter == 0:
                 print(f"- {task_type}.{self.Name}: " + "%.5f"%self.Total[task_type])
-                # if self.Name == "ProtoNet.forward" and task_type=="Validate":
-                #     print("- Validate.ProtoNet.forward called")
-                    # print(f"- Validate.ProtoNet.forward: {self.Iter['Validate']}")
                 self._clear()
 
             return res
 
         return wrapped_func
+
+
+class FuncProfiler:
+    def __init__(self, name, report_iter=None):
+        self.Name = name
+        self.Iter = {'Train': 0, 'Validate': 0, 'Test': 0}
+        self.Total = {'Train': 0, 'Validate': 0, 'Test': 0}
+        self.ReportIter = {'Train': config.train.ValCycle,
+                           'Validate': config.train.ValEpisode,
+                           'Test': config.test.ReportIter}
+        if report_iter is not None:
+            self.ReportIter = report_iter
+
+    def _clear(self):
+        self.Total = {'Train': 0, 'Validate': 0, 'Test': 0}
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def wrapped_func(*args, **kwargs):
+            task_type = kwargs.get('task_type', None)
+            if task_type is None:
+                return func(*args, **kwargs)
+
+            self.Iter[task_type] += 1
+
+            start = time.time()
+            res = func(*args, **kwargs)
+            end = time.time()
+            self.Total[task_type] += (end - start)
+
+            iters = self.Iter[task_type]
+            report_iter = self.ReportIter[task_type]
+
+            if iters % report_iter == 0:
+                print(f"- {task_type}.{self.Name}: " + "%.5f" % self.Total[task_type])
+                self._clear()
+
+            return res
+
+        return wrapped_func
+
+
