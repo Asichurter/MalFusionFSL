da0dc3fdbd4fca0cd0405deebf789f8dbbbf994c
Asichurter <asichurter@gmail.com>
2021-03-20 17:36:58 +0800

    refactor fusion structure, add Hadamard Product Bilinear fusion

--- comp/nn/fusion/linear_fusion.py
+++ comp/nn/fusion/linear_fusion.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch
 
 
 ###########################################
@@ -6,16 +7,56 @@ from torch import nn
 # 最好还是在之前经过一次重投影
 ###########################################
 class BilinearFusion(nn.Module):
-    def __init__(self, seq_dim, img_dim, output_dim=256):
+
+    def __init__(self, seq_dim, img_dim, output_dim=256, bili_norm_type='bn', bili_affine=False):
         super(BilinearFusion, self).__init__()
         self.Trans = nn.Bilinear(seq_dim, img_dim, output_dim, bias=False)
+
         # 使用一个标准化层来约束输出，防止输出值过大
-        self.Norm = nn.BatchNorm1d(output_dim)
+        if bili_norm_type == 'bn':
+            self.Norm = nn.BatchNorm1d(output_dim, affine=bili_affine)
+        elif bili_norm_type == 'ln':
+            # LayerNorm的标准化范围只有输出维度
+            self.Norm = nn.LayerNorm(output_dim, elementwise_affine=bili_affine)
+        else:
+            raise ValueError(f'[BilinearFusion] Unrecognized normalization type: {bili_norm_type}')
 
     def forward(self, seq_features, img_features, **kwargs):
         fused_features = self.Trans(seq_features, img_features)
-        return_size = fused_features.size()
-        feature_dim = return_size[-1]
-        # Norm的输入必须是[batch, feature]形状的，多余的维度需要被合并
-        fused_features = self.Norm(fused_features.view(-1, feature_dim))
-        return fused_features.view(return_size)
+        fused_features = self.Norm(fused_features)
+
+        return fused_features
+
+
+################################################
+# 使用Hadamard积作为低秩约束的双线性特征融合的分解，用
+# 于减少参数数量。
+# 参见论文: "Hadamard Product for Low-Rank Bilinear
+# Pooling"
+################################################
+class HdmProdBilinearFusion(nn.Module):
+
+    def __init__(self, seq_dim, img_dim, hidden_dim, output_dim,
+                 bili_norm_type=None, bili_affine=False):
+        super(HdmProdBilinearFusion, self).__init__()
+
+        self.SeqTrans = nn.Linear(seq_dim, hidden_dim)
+        self.ImgTrans = nn.Linear(img_dim, hidden_dim)
+        self.OutTrans = nn.Linear(hidden_dim, output_dim)
+
+        if bili_norm_type is None:
+            self.Norm = nn.Identity()
+        elif bili_norm_type == 'bn':
+            self.Norm = nn.BatchNorm1d(output_dim, affine=bili_affine)
+        elif bili_norm_type == 'ln':
+            self.Norm = nn.LayerNorm(output_dim, elementwise_affine=bili_affine)
+        else:
+            raise ValueError(f'[HdmProdBilinearFusion] Unrecognized normalization type: {bili_norm_type}')
+
+    def forward(self, seq_features, img_features, **kwargs):
+        prod = self.SeqTrans(seq_features) * self.ImgTrans(img_features)
+        prod = torch.tanh(prod)     # 使用tanh激活Hadamard积
+        return self.Norm(self.OutTrans(prod))
+
+
+
